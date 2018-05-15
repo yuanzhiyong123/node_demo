dc796ad8ef2ae1e16dd40143846ceeebb4ef65fa
yuanzhiyong <1060915552@qq.com>
2018-05-15 23:58:12 +0800

    add

--- 16_fs-promisify.js
+++ 16_fs-promisify.js
@@ -3,8 +3,11 @@ const promisify = require('util').promisify;
 
 const  readFile = promisify(fs.readFile);  //promise 化  大于v8.0版本
 
+
 readFile('./16_fs-promisify.js').then((data) => {  
     console.log(data);
+    
 }).catch((err) => {
     console.log(err);
 });
+// console.log(process.cwd());
---
+++ app/app.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const chalk = require('chalk');
+const path = require('path');
+const fs = require('fs');
+const promisify = require('util').promisify;
+const stat = promisify(fs.stat);
+const files = promisify(fs.readdir);
+
+
+const config = {
+    host: '127.0.0.1',
+    port: 8081,
+    root: process.cwd()
+}
+const server = http.createServer((req, res) => {
+    const filePath = path.join(config.root, req.url);
+    res.statusCode = 200;
+    stat(filePath).then((stat) => {
+        if (stat.isDirectory()) {
+            files(filePath).then((files) => {
+                res.setHeader('Content-type', 'text/plain');
+                res.end(files.join(','));
+            }).catch(err => {
+                console.log(err);
+                res.setHeader('Content-type', 'text/plain');
+                res.end(`${filePath} is no a directory or a file`);
+            });
+        } else if (stat.isFile()) {
+            fs.createReadStream(filePath).pipe(res);
+        }
+    }).catch((err) => {
+        console.log(err);
+        res.setHeader('Content-type', 'text/plain');
+        res.end(`${filePath} is no a directory or a file`);
+    });
+    fs.stat(filePath, (err, stat) => {
+        if (err) {
+
+        }
+
+    });
+});
+
+server.listen(config.port, config.host, () => {
+    const addr = `http://${config.host}:${config.port}`
+    console.log(`run at ${chalk.red(addr)}`);
+});
+
