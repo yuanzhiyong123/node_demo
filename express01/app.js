529976462abf7f082a0799200ad585abcccdcfce
yuanzhiyong  <yuanzhiyong@unioncontrol.com>
2018-05-22 17:44:52 +0800

    add

--- express01/app.js
+++ express01/app.js
@@ -1,19 +1,31 @@
 const express = require('express');
-const app = express();
 const MongoClient = require('mongodb').MongoClient;
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
-const dbUrl = 'mongodb://127.0.0.1:27017';
+const app = express();
+
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));  //静态服务
+app.use(cookieParser());
+app.use(session({
+    secret: 'keyboard cat',  //随意字符串  用于session签名
+    resave: false,
+    saveUninitialized: true
+    // cookie: { secure: true }
+  }))
+
+const dbUrl = 'mongodb://127.0.0.1:27017';
 
 app.use((req, res, next) => {
-    console.log(new Date());
     next();
 });
 
 app.get('/', (req, res) => {
-    MongoClient.connect(dbUrl, (err, client) => {
+    // res.cookie('username','zhiyong');
+    // req.session.userInfo = 'nihao';
+   /* MongoClient.connect(dbUrl, (err, client) => {
         if(err){
             console.log(err);
             return;
@@ -28,8 +40,22 @@ app.get('/', (req, res) => {
                 list: data
             }
             res.render('index', obj);
+            client.close();
         });
-    });
+    });*/
+    res.render('index');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/add', (req, res) => {
+    res.render('edit');
+});
+
+app.get('/productList', (req, res) => {
+    res.render('index');
 });
 
 app.use((req,res, next) => {
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,22 +4,27 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://127.0.0.1:27017/demos'
+var dbUrl = 'mongodb://127.0.0.1:27017'
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.status(200);
   res.header("Access-Control-Allow-Origin", "*");
-  var path2 = path.join(__dirname,'../api/index.json');
-  MongoClient.connect({ url: true }, (err, db) => {
-    if(err) {throw err}
-    db.collection('goods').insertOne({
-      name: "zhaowu",
-      age: 30
-    }, (err, res) => {
-      if(err) throw err;
-      res.end('插入数据库成功！'); 
-    })
+  var path2 = path.join(__dirname, '../api/index.json');
+  MongoClient.connect(dbUrl, (err, client) => {
+    if (err) { throw err }
+    var db = client.db('demos');
+    db.collection('goods').find({}).toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      var obj = {
+        data
+      }
+      console.log(data);
+      res.json(obj);
+    });
   })
 });
 
